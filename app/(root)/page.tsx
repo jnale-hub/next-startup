1e31723ea41a7a2ebf2dbb1872caeb08fb0163e9
John Mark Delima <delimajohnmark14@gmail.com>
2025-01-29 19:33:10 +0800

    Add sample posts data to Home component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      title: "Startup 1",
+      description: "Description 1",
+      views: 10,
+      image: "https://unsplash.com/photos/a-computer-generated-image-of-a-curved-surface-O8s_2_pEISg",
+      category: "Tech",
+      _createdAt: new Date(),
+      _id: "1",
+      author: { id: "1" , name: "John"},
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
