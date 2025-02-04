ace7242d6f45dbc579352d6189c5b106f933f49f
John Mark Delima <delimajohnmark14@gmail.com>
2025-02-04 10:58:29 +0800

    Refactor view count increment logic for improved readability

--- components/View.tsx
+++ components/View.tsx
@@ -8,9 +8,13 @@ const View = async ({ id }: { id: string }) => {
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  await writeClient.patch(id).set({views: totalViews + 1}).commit()
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+        
   return (
-    <div className="view-container"> 
+    <div className="view-container">
       <div className="absolute -top-2 -right-2">
         <Ping />
       </div>
