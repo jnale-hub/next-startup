aaef50370f87e6f23254699f53ddf11ab34e98da
John Mark Delima <delimajohnmark14@gmail.com>
2025-02-10 11:22:30 +0800

    Add parseServerActionResponse utility function for handling server responses

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,11 +5,15 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-
 export function formatDate(date: string) {
   return new Date(date).toLocaleDateString("en-US", {
     month: "long",
     day: "numeric",
     year: "numeric",
   });
+}
+
+
+export function parseServerActionResponse<T>(response: T) {
+  return JSON.parse(JSON.stringify(response));
 }
