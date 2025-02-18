fd524f19b9c4dcc034044e19b0bb181dcc43e80d
John Mark Delima <delimajohnmark14@gmail.com>
2025-02-18 10:13:22 +0800

    Enable experimental partial page rendering for user page

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -7,6 +7,8 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
 
+export const experimental_ppr = true;
+
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const session = await auth();
