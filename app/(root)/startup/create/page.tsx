0962cb6d0d026d2a899bf8a8cbab1d80468b8cad
John Mark Delima <delimajohnmark14@gmail.com>
2025-02-19 17:20:39 +0800

    Fix class name bug

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -10,7 +10,7 @@ const PageProps = async () => {
 
   return ( 
     <>
-      <section className="pink_xcontainer !min-h-[230px]">
+      <section className="pink_container !min-h-[230px]">
         <h1 className="heading">Submit Your Startup</h1>
       </section>
 
