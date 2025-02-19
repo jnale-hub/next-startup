ebb10510102b0c1d39dbad6b99160f09cb9fb1e8
John Mark Delima <delimajohnmark14@gmail.com>
2025-02-19 17:21:19 +0800

    Refactor view update logic to use Next.js after middleware and improve view text styling

--- components/View.tsx
+++ components/View.tsx
@@ -2,17 +2,21 @@ import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
+  after(
+    async () =>
       await writeClient
         .patch(id)
         .set({ views: totalViews + 1 })
-        .commit()
-        
+        .commit(),
+  );
+
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
@@ -20,7 +24,7 @@ const View = async ({ id }: { id: string }) => {
       </div>
 
       <p className="view-text">
-        <span className="font-black">Views: {totalViews}</span>
+        <span className="font-bold">Views: {totalViews}</span>
       </p>
     </div>
   );
