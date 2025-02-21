2158d607093e10202de3704f93167dc9eccd77d3
John Mark Delima <delimajohnmark14@gmail.com>
2025-02-21 14:06:47 +0800

    Update Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,13 +8,14 @@ const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+    <header className="px-5 py-3 shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} />
+          {/* <Image src="/logo.png" alt="logo" width={144} height={30} /> */}
+          <span>OpenSourceHub</span>
         </Link>
 
-        <div className="flex items-center gap-5 text-black">
+        <div className="flex items-center gap-5">
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
